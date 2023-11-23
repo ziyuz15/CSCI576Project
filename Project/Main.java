5e0c6ffdb7ff2a4bd7067bd6321ff67df7475b62
WenzhePan <wenzhepa@usc.edu>
2023-11-22 21:54:19 -0800

    BUG FIXED

--- Project/Main.java
+++
@@ -1,9 +0,0 @@
-//package videoProcessing.src;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        VideoProcessor videoProcessor = new VideoProcessor();
-        VideoProcessor.processVideo("D:\\USC\\CSCI576\\Videos\\video3.mp4");
-    }
-}
