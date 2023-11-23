b17e10ee7024a67fc06f42aa4b2b16ffc9c45715
PennyZhai <81480243+PennyZhai@users.noreply.github.com>
2023-11-23 00:02:00 -0800

    Provide usage of AudioProcess

--- Project/src/main/java/org/example/Main.java
+++ Project/src/main/java/org/example/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
         VideoProcessor videoProcessor = new VideoProcessor();
         VideoProcessor.processVideo("D:\\USC\\CSCI576\\Videos\\video3.mp4");
+
+//        AudioProcess myAudio = new AudioProcess("/Users/qianxi/eclipse-workspace/testPro/src/");
+//        myAudio.processAudio("video8_1.wav");
     }
 }
